3f22ce69d0fe92c1563db11d050fc796d8cc0abf
Michael Tuszynski <miketuszynski42@gmail.com>
2023-10-17 13:18:21 -0700

    first working front and back end

--- main.tf
+++ main.tf
@@ -53,6 +53,18 @@ resource "google_artifact_registry_repository" "backend" {
   }
 }
 
+resource "google_artifact_registry_repository" "frontend" {
+  project       = var.project_name
+  location      = var.region
+  repository_id = var.repository_name_frontend
+  description   = "Frontend repository"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = false
+  }
+}
+
 resource "google_project_iam_member" "artifact_registry_reader" {
   project = var.project_name
   role    = "roles/artifactregistry.reader"
@@ -65,15 +77,21 @@ resource "google_project_iam_member" "artifact_registry_writer" {
   member  = "serviceAccount:${google_service_account.admin_sa.email}"
 }
 
-resource "github_actions_secret" "deployment_secret" {
+resource "github_actions_secret" "deployment_secret_backend" {
   repository      = var.repository_name_backend
+  secret_name     = "GCP_SA_KEY"
+  plaintext_value = base64decode(google_service_account_key.admin_sa_key.private_key)
+}
+
+resource "github_actions_secret" "deployment_secret_frontend" {
+  repository      = var.repository_name_frontend
   secret_name     = "GCP_SA_KEY"
   plaintext_value = base64decode(google_service_account_key.admin_sa_key.private_key)
 }
 
 resource "github_repository_file" "backend_workflow" {
 
-  depends_on = [github_actions_secret.deployment_secret, google_artifact_registry_repository.backend]
+  depends_on = [github_actions_secret.deployment_secret_backend, google_artifact_registry_repository.backend]
 
   overwrite_on_create = true
   repository          = var.repository_name_backend
@@ -90,7 +108,7 @@ resource "github_repository_file" "backend_workflow" {
 
     jobs:
       push_to_registry:
-        name: Build and push Docker image
+        name: Build Backend API
         runs-on: ubuntu-latest
 
         steps:
@@ -100,12 +118,10 @@ resource "github_repository_file" "backend_workflow" {
             - name: Set up Docker Buildx
               uses: docker/setup-buildx-action@v1
 
-            - name: Set up Google Cloud SDK
-              uses: google-github-actions/setup-gcloud@v1
+            - name: Setup GCP Authentication
+              uses: google-github-actions/auth@v1
               with:
-                service_account_key: $${{ secrets.GCP_SA_KEY }}
-                project_id: ${var.project_name}
-                export_default_credentials: true
+                credentials_json: $${{ secrets.GCP_SA_KEY }}
 
             - name: Login to Artifact Registry
               uses: docker/login-action@v1
@@ -121,6 +137,12 @@ resource "github_repository_file" "backend_workflow" {
                 file: ./Dockerfile
                 push: true
                 tags: ${var.region}-docker.pkg.dev/${var.project_name}/${var.repository_name_backend}/${var.repository_name_backend}:latest
+
+            - name: Deploy to Cloud Run
+              uses: 'google-github-actions/deploy-cloudrun@v1'
+              with:
+                service: "${var.prefix}-api"
+                image: ${var.region}-docker.pkg.dev/${var.project_name}/${var.repository_name_backend}/${var.repository_name_backend}:latest
     EOF
 }
 
@@ -297,3 +319,109 @@ resource "google_cloud_run_service_iam_member" "public_access" {
 }
 
 
+
+
+resource "google_storage_bucket" "static_website_bucket" {
+  name          = "${var.prefix}-static-website-bucket"
+  location      = "US"
+  force_destroy = true
+  project       = var.project_name
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+resource "google_storage_bucket_iam_member" "bucket_iam_member" {
+  bucket = google_storage_bucket.static_website_bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.admin_sa.email}"
+}
+
+resource "google_storage_bucket_iam_member" "public_read" {
+  bucket = google_storage_bucket.static_website_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
+
+resource "github_repository_file" "frontend_workflow" {
+
+  depends_on = [
+    github_actions_secret.deployment_secret_frontend,
+    google_artifact_registry_repository.frontend,
+    google_storage_bucket.static_website_bucket,
+    google_cloud_run_service.api_service
+  ]
+
+
+  overwrite_on_create = true
+  repository          = var.repository_name_frontend
+  branch              = var.repository_branch_frontend
+  file                = ".github/workflows/fe-workflow.yml"
+  content             = <<-EOF
+    name: CI/CD Pipeline
+
+    on:
+      push:
+        branches:
+          - ${var.repository_branch_frontend}
+
+
+    jobs:
+      push_to_gcp_bucket:
+        name: Build react website for GCP
+        runs-on: ubuntu-latest
+
+        steps:
+            - name: Checkout code
+              uses: actions/checkout@v3
+
+            - name: Set up Node.js
+              uses: actions/setup-node@v3
+              with:
+                node-version: '18'
+
+            - name: Install dependencies
+              run: yarn install
+
+            - name: Build
+              run: yarn build
+
+            - name: Create config.json
+              run: |
+                echo '{
+                  "REACT_APP_BACKEND_URL": "${replace(google_cloud_run_service.api_service.status[0].url, "https://", "")}"
+                }' > build/config.json
+
+            - name: Setup GCP Authentication
+              uses: google-github-actions/auth@v1
+              with:
+                credentials_json: $${{ secrets.GCP_SA_KEY }}
+
+            - name: Upload build folder to GCP bucket
+              uses: 'google-github-actions/upload-cloud-storage@v1'
+              with:
+                path: 'build'
+                destination: '${google_storage_bucket.static_website_bucket.name}'
+                parent: false
+  EOF
+}
+
+data "http" "dispatch_event_frontend" {
+
+  url    = "https://api.github.com/repos/${var.github_username}/${var.repository_name_frontend}/dispatches"
+  method = "POST"
+
+  request_headers = {
+    Accept        = "application/vnd.github.everest-preview+json"
+    Authorization = "token ${var.github_token}"
+  }
+
+  request_body = jsonencode({
+    event_type = "my-event"
+  })
+
+  depends_on = [github_repository_file.frontend_workflow]
+}
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,10 @@
 // Outputs: Display the deployed API URL
 output "api_url" {
+  description = "The URL of the deployed API."
   value = google_cloud_run_service.api_service.status[0].url
+}
+
+output "website_url" {
+  description = "The URL of the static website."
+  value       = "https://${google_storage_bucket.static_website_bucket.name}.storage.googleapis.com/"
 }
