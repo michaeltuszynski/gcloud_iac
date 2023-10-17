df3c2bd2c235333a360bbf9bb5e0dcda54be6d12
Michael Tuszynski <miketuszynski42@gmail.com>
2023-10-17 09:44:45 -0700

    first working gcp backend

--- versions.tf
+++ versions.tf
@@ -7,26 +7,14 @@ terraform {
     github = {
       source = "integrations/github"
     }
-    # google-beta = {
-    #   source  = "hashicorp/google-beta"
-    #   version = "~> 3.0"
-    # }
   }
 }
 
 provider "google" {
-  #credentials = file("terraform-admin-402218-d12b6eae1339.json")
   credentials = file("gcp-creds-v2.json")
   region      = "us-central1"
   zone        = "us-central1-a"
 }
-
-# provider "google-beta" {
-#   credentials = file("terraform-admin-402218-d12b6eae1339.json")
-#   project     = "terraform-admin-402218"
-#   region      = "us-central1"
-#   zone = "us-central1-a"
-# }
 
 provider "github" {
   token = var.github_token
