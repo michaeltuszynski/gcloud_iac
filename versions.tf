765f23d932e20bfaf791fcaedba109538190814e
Michael Tuszynski <miketuszynski42@gmail.com>
2023-10-19 12:53:50 -0700

    remove google beta

--- versions.tf
+++ versions.tf
@@ -7,20 +7,10 @@ terraform {
     github = {
       source = "integrations/github"
     }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 3.0"
-    }
   }
 }
 
 provider "google" {
-  credentials = file("gcp-creds-v2.json")
-  region      = "us-central1"
-  zone        = "us-central1-a"
-}
-
-provider "google-beta" {
   credentials = file("gcp-creds-v2.json")
   region      = "us-central1"
   zone        = "us-central1-a"
